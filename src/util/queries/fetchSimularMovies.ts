5aafc96538ddf6c392effc4faa7996a85afcf852
Ben Belcher <75249728+BenjaminBelchie@users.noreply.github.com>
2023-12-16 15:45:56 +0000

    Updated the API request to get similar movies (#10)

--- src/util/queries/fetchSimularMovies.ts
+++ src/util/queries/fetchSimularMovies.ts
@@ -4,7 +4,7 @@ import { type DiscoverMoviesResponse } from "~/types/Responses/DiscoverMovies";
 
 export default async function fetchSimularMovies(movieID: string) {
   const simularMoviesResponse: DiscoverMoviesResponse = await axios.get(
-    `https://api.themoviedb.org/3/movie/${movieID}/similar`,
+    `https://api.themoviedb.org/3/movie/${movieID}/recommendations`,
     {
       headers: { Authorization: `Bearer ${env.TMDB_READ_ACCESS_TOKEN}` },
     },
